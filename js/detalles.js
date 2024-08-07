765c0e08b8747a530fdbe97ccde266882dccefd8
Ains9803 <eleinercespedes@gmail.com>
2024-08-07 04:29:02 -0400

    Actualización de  múltiples archivos: mejora de estilos y cambios en la estructura de HTML

--- js/detalles.js
+++ js/detalles.js
@@ -1,6 +1,14 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 
+function truncateText(text, wordLimit) {
+  const words = text.split(' '); 
+  if (words.length > wordLimit) {
+    return words.slice(0, wordLimit).join(' ') + '...'; 
+  }
+  return text; 
+}
+
 fetch(`https://kitsu.io/api/edge/anime/${id}`)
   .then(response => {
     if (!response.ok) {
@@ -9,15 +17,22 @@ fetch(`https://kitsu.io/api/edge/anime/${id}`)
     return response.json();
   })
   .then(data => {
+    console.log(data); 
     if (data && data.data) {
       const anime = data.data; 
       document.getElementById('title').textContent = anime.attributes.canonicalTitle;
+
       document.getElementById('details').innerHTML = `
-        <img src="${anime.attributes.posterImage.original}" alt="${anime.attributes.canonicalTitle}" />
-        <p>${anime.attributes.synopsis}</p>
+        <img src="${anime.attributes.posterImage.original}" alt="${anime.attributes.canonicalTitle}" />        
+        <p>Tipo: <b>${anime.attributes.subtype || 'Desconocido'}</b></p>
+        <p>Estreno: <b>${anime.attributes.startDate ? new Date(anime.attributes.startDate).toLocaleDateString('es-ES', { day: 'numeric', month: 'long', year: 'numeric' }) : 'Desconocido'}</b></p>
+        <p>Episodios: <b>${anime.attributes.episodeCount || 'Desconocido'}</b></p>
+        <p>Género: <b>${anime.relationships.genres && anime.relationships.genres.data ? anime.relationships.genres.data.map(genre => genre.attributes.name).join(', ') : 'Desconocido'}</b></p>        
+        <p><b>Sinopsis: </b>${truncateText(anime.attributes.synopsis, 50)}</p>
+        
       `;
 
-      // Obtener el ID del video de YouTube
+      
       const youtubeVideoId = anime.attributes.youtubeVideoId; 
       if (youtubeVideoId) {
         const videoContainer = document.querySelector('.videos');
